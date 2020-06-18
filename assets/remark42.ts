37d89f1a4d9ba5d9aefe59b74213e96a572f7162
Pacharapol Withayasakpunt <polv@polv.cc>
2020-06-18 11:38:52 +0700

    🐛 Fix: fix commenting engine

--- assets/remark42.ts
+++ assets/remark42.ts
@@ -9,7 +9,7 @@ export function initRemark42 (config: any, url: string) {
     //     - 'embed': basic comments widget
     //     - 'last-comments': last comments widget, see `Last Comments` section below
     //     - 'counter': counter widget, see `Counter` section below
-    url, // optional param; if it isn't defined
+    url // optional param; if it isn't defined
     // `window.location.origin + window.location.pathname` will be used,
     //
     // Note that if you use query parameters as significant part of url
@@ -28,10 +28,10 @@ export function initRemark42 (config: any, url: string) {
     //
     // If you deal with query parameters make sure you pass only significant part of it
     // in well defined order
-    max_shown_comments: config.maxShownComments, // optional param; if it isn't defined default value (15) will be used
-    theme: config.theme, // optional param; if it isn't defined default value ('light') will be used
+    // max_shown_comments: config.maxShownComments, // optional param; if it isn't defined default value (15) will be used
+    // theme: config.theme, // optional param; if it isn't defined default value ('light') will be used
     // page_title: 'Moving to Remark42', // optional param; if it isn't defined `document.title` will be used
-    locale: config.locale // set up locale and language, if it isn't defined default value ('en') will be used,
+    // locale: config.locale // set up locale and language, if it isn't defined default value ('en') will be used,
   }
 
   // eslint-disable-next-line camelcase
--- components/PostFull.tsx
+++ components/PostFull.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link'
-import { Component } from 'react'
+import { useEffect } from 'react'
 
 import { fixDOM } from '@/assets/fix-html'
 import { initRemark42 } from '@/assets/remark42'
@@ -10,114 +10,129 @@ import { IPost } from '@/types/post'
 
 import PostHeader from './PostHeader'
 
-interface IProp {
-  post: IPost
-}
-
-export default class PostFull extends Component<IProp> {
-  render () {
-    const { post } = this.props
-
-    return (
-      <section>
-        <article className={sMargin['my-1']}>
-          <div className="card-content content">
-            <PostHeader post={post} />
-            <h1 className="title">{post.title}</h1>
-
-            {post.image ? (
-              <div className="image-full">
-                <img src={post.image} alt={post.title} />
-              </div>
-            ) : null}
-
-            <div dangerouslySetInnerHTML={{ __html: post.html }}></div>
-            <div className={sExtra['break-word']}>
-              <span>Tags: {(post.tag || []).map((t: string) => (
-                <span className="u-tag" key={t}>
-                  <Link href={`/tag/${t}`}><a>{t}</a></Link>
-                </span>
-              ))}</span>
-            </div>
-          </div>
-        </article>
-
-        <footer className={['card', sMargin['my-1']].join(' ')}>
-          <div className="card-content">
-            <div id="remark42"></div>
-          </div>
-        </footer>
-
-        <style jsx>{`
-        .u-tag + .u-tag {
-          margin-left: 0.5em;
-        }
-
-        .image-full {
-          text-align: center;
-          margin: 1rem;
-        }
-
-        .image-full img {
-          min-width: 500px;
-          width: auto;
-        }
-
-        @media only screen and (max-width: 800px) {
-          .image-full {
-            margin-left: -1.5rem;
-            margin-right: -1.5rem;
-          }
-
-          .image-full img {
-            min-width: unset;
-            width: auto;
-          }
-        }
-        `}</style>
-      </section>
-    )
+declare global {
+  interface Window {
+    remark42Observer: MutationObserver
   }
 }
 
-if (typeof window !== 'undefined') {
-  const observer = new MutationObserver((muts) => {
-    muts.map((mut) => {
-      mut.addedNodes.forEach((n) => {
-        if (n instanceof HTMLElement) {
-          const mainEl = n.querySelector('main')
-          if (mainEl) {
-            fixDOM(n)
-          }
+const PostFull = ({ post }: {
+  post: IPost
+}) => {
+  useEffect(() => {
+    if (!window.remark42Observer) {
+      window.remark42Observer = new MutationObserver((muts) => {
+        muts.map((mut) => {
+          mut.addedNodes.forEach((n) => {
+            if (n instanceof HTMLElement) {
+              const mainEl = n.querySelector('main')
+              if (mainEl) {
+                fixDOM(n)
+              }
 
-          const remarkEl = n.querySelector('#remark42')
+              const remarkEl = n.querySelector('#remark42')
 
-          if (remarkEl) {
-            const { REMARK42 } = window as any
-            if (REMARK42) {
-              REMARK42.destroy()
+              if (remarkEl) {
+                const { REMARK42 } = window as any
+                if (REMARK42) {
+                  REMARK42.destroy()
+                }
+
+                initRemark42(themeConfig.comment.remark42, location.origin + location.pathname)
+              }
             }
+          })
 
-            initRemark42(themeConfig.comment.remark42, location.origin + location.pathname)
-          }
-        }
+          mut.removedNodes.forEach((n) => {
+            if (n instanceof HTMLElement) {
+              const remarkEl = n.querySelector('#remark42')
+              const { REMARK42 } = window as any
+              if (REMARK42) {
+                REMARK42.destroy()
+              }
+            }
+          })
+        })
       })
 
-      mut.removedNodes.forEach((n) => {
-        if (n instanceof HTMLElement) {
-          const remarkEl = n.querySelector('#remark42')
-          const { REMARK42 } = window as any
-          if (REMARK42) {
-            REMARK42.destroy()
-          }
-        }
+      window.remark42Observer.observe(document.body, {
+        childList: true,
+        characterData: true,
+        subtree: true
       })
-    })
-  })
 
-  observer.observe(document.body, {
-    childList: true,
-    characterData: true,
-    subtree: true
-  })
+      const remarkEl = document.querySelector('#remark42')
+
+      if (remarkEl) {
+        const { REMARK42 } = window as any
+        if (REMARK42) {
+          REMARK42.destroy()
+        }
+
+        initRemark42(themeConfig.comment.remark42, location.origin + location.pathname)
+      }
+    }
+  }, [])
+
+  return (
+    <section>
+      <article className={sMargin['my-1']}>
+        <div className="card-content content">
+          <PostHeader post={post} />
+          <h1 className="title">{post.title}</h1>
+
+          {post.image ? (
+            <div className="image-full">
+              <img src={post.image} alt={post.title} />
+            </div>
+          ) : null}
+
+          <div dangerouslySetInnerHTML={{ __html: post.html }}></div>
+          <div className={sExtra['break-word']}>
+            <span>Tags: {(post.tag || []).map((t: string) => (
+              <span className="u-tag" key={t}>
+                <Link href={`/tag/${t}`}><a>{t}</a></Link>
+              </span>
+            ))}</span>
+          </div>
+        </div>
+      </article>
+
+      <footer className={['card', sMargin['my-1']].join(' ')}>
+        <div className="card-content">
+          <div id="remark42"></div>
+        </div>
+      </footer>
+
+      <style jsx>{`
+      .u-tag + .u-tag {
+        margin-left: 0.5em;
+      }
+
+      .image-full {
+        text-align: center;
+        margin: 1rem;
+      }
+
+      .image-full img {
+        min-width: 500px;
+        width: auto;
+      }
+
+      @media only screen and (max-width: 800px) {
+        .image-full {
+          margin-left: -1.5rem;
+          margin-right: -1.5rem;
+        }
+
+        .image-full img {
+          min-width: unset;
+          width: auto;
+        }
+      }
+      `}</style>
+    </section>
+  )
 }
+
+export default PostFull
